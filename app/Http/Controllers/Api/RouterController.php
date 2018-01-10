7fc8fccad071e1cbf454ce15cf9cbf0650fa02d0
HeRedBo <hhbjkd@163.com>
2018-01-10 15:49:51 +0800

    修改路由配置;

--- app/Http/Controllers/Api/RouterController.php
+++ app/Http/Controllers/Api/RouterController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\ApiServer\Server;
+use App\Services\ApiServer\Error;
 
+/**
+ * Api 入口控制器
+ */
 class RouterController extends Controller
 {
-    //
+    /**
+     * API总入口文件
+     */
+    public function index()
+    {
+        $server = new Server(new Error);
+        return $server->run();
+    }
 }
